e6c2d6f866e12195e0cc1bb085adaa00d07d8b1a
Eric Pesch <ericpes@umich.edu>
2012-03-22 09:35:31 -0400

    Test changes for toeButtonGroup.h

--- toeButtonGroup.h
+++ toeButtonGroup.h
@@ -36,7 +36,7 @@ class toeButtonGroup: public QButtonGroup
         else if(button->text() == "toe 5")
             box->setText("This little piggy went wee wee wee all the way home.");
         else if(button->text() == "toe 6")
-            box->setText("This little piggy had 6 toes.");
+            box->setText("This little piggy had 6 toes, and now we see if he can actually do anything...");
         box->show();
     }
 
